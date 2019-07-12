1c5a1fb9a358845f4c96c62519cd4d263f6c8c4b
XieWei <wuxiaoshen@shu.edu.cn>
2019-07-12 11:14:04 +0800

    LOGGER

--- EnterpriseWithHttpRouter/tencent_vote/pkg/middleware/logger.go
+++ EnterpriseWithHttpRouter/tencent_vote/pkg/middleware/logger.go
@@ -9,7 +9,7 @@ import (
 
 func Logger(next http.HandlerFunc) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		Message := fmt.Sprintf("HOST: %s | URL: %s | TIMES: %s | METHOD: %s", request.Host, request.RequestURI, time.Now().Format("2006-01-02 15:04:05"), request.Method)
+		Message := fmt.Sprintf("[HTTP_ROUTER_LOGGER]: HOST: %s | URL: %s | TIMES: %s | METHOD: %s", request.Host, request.RequestURI, time.Now().Format("2006-01-02 15:04:05"), request.Method)
 		log.Printf(Red(Message))
 		next.ServeHTTP(writer, request)
 	}
