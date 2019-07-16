3d2e4a3cc6c416a9e92101aef9f64390cd21f4a8
XieWei <wuxiaoshen@shu.edu.cn>
2019-07-16 11:57:38 +0800

    script

--- EnterpriseWithHTTP/tencent_vote/script/ping.sh
+++ EnterpriseWithHTTP/tencent_vote/script/ping.sh
@@ -5,4 +5,33 @@
 2, ping web server
 EOF
 
-http localhost:7201/ping
+results=`http localhost:7201/ping`
+
+echo ${results}
+echo ""
+
+function GetCode() {
+    echo ${results:8:3}
+}
+
+function GetData() {
+    echo ${results:20:4}
+}
+
+command=${1}
+case ${command} in
+code) GetCode;;
+data) GetData;;
+esac;
+
+
+Search="https://localhost:7201/v1/api/votes"
+
+function GetPartOne() {
+    echo ${Search#*/}
+    echo ${Search##*/}
+    echo ${Search%/*}
+    echo ${Search%%/*}
+    echo ${Search} | cut -f 1,2 -d '/'
+}
+GetPartOne
--- EnterpriseWithHTTP/tencent_vote/script/run.sh
+++ EnterpriseWithHTTP/tencent_vote/script/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
+:<<EOF
+1. project script
+2. run
+3. build
+EOF
 echo "Start Web Server..."
 
 while read line
@@ -7,8 +11,58 @@ do
     l=`echo ${line} | awk '{print $0}'`
     echo ${l}
 done < ../main.go
-go run ../main.go
+
+echo "Read File Done..."
+
+target="tencent_vote"
+readonly target
+
+function build() {
+    echo "go build -o ${target} ../main.go"
+    `go build -o ${target} ../main.go`
+}
+
+function runWithGo() {
+    echo "go run ../main.go"
+    `go run ../main.go`
+}
+
+function runWithBinary(){
+    echo "execute binary file"
+    if [[ ! -e ${target} ]];then
+        build
+        ./${target}
+    else
+        ./${target}
+    fi
+}
 
 
+function Remove() {
+    if [[ -e ${target} ]]; then
+        rm ${target}
+    else
+        echo "Success!"
+    fi
+}
 
+function Commands() {
+    commands=("run" "build" "start" "remove" "list")
+    echo ${commands[@]}
+    echo ${commands[*]}
+    echo ${#commands[*]}
+    echo ${#commands[@]}
+    for i in ${commands[@]}
+    do
+        echo ${i}
+    done
+}
 
+command=${1}
+case ${command} in
+run) runWithGo;;
+build) build;;
+start) runWithBinary;;
+remove) Remove;;
+list) Commands;;
+esac
